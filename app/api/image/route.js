59586cfb27fd284f8cc0e53e4eb23c507f254560
zhourentian <15970334351@163.com>
2024-12-04 17:15:42 +0800

    update：路由处理程序

---
+++ app/api/image/route.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: zhourentian 15970334351@163.com
+ * @Date: 2024-12-04 16:43:01
+ * @LastEditors: zhourentian 15970334351@163.com
+ * @LastEditTime: 2024-12-04 16:45:43
+ * @FilePath: \demo\next-app\app\api\image\route.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+export async function GET() {
+    const res = await fetch('https://api.thecatapi.com/v1/images/search', {
+        next: { revalidate: 5 },
+    })
+    const data = await res.json()
+    console.log(data)
+    return Response.json(data)
+}
