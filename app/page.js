540336295c74cb7b18a6549abf93851aa84f1bd5
zhourentian <15970334351@163.com>
2025-01-03 10:34:11 +0800

    feat：缓存

--- app/page.js
+++ app/page.js
@@ -22,17 +22,56 @@
 // }
 
 
-async function getData() {
-  const res = await fetch('https://api.thecatapi.com/v1/images/search', { next: { tags: ['collection'] } })
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
+// async function getData() {
+//   const res = await fetch('https://api.thecatapi.com/v1/images/search', { next: { tags: ['collection'] } })
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch data')
+//   }
 
+//   return res.json()
+// }
+
+// export default async function Page() {
+//   const data = await getData()
+
+//   return <img src={data[0].url} width="300" />
+// }
+
+
+
+// 缓存
+async function getData() {
+  // 接口每次调用都会返回一个随机的猫猫图片数据
+  // 因为设置了 fetch 不缓存，页面自动从静态渲染转为动态渲染，所以每次刷新，接口都会返回新的图片。
+  const { signal } = new AbortController()
+  const res = await fetch('https://api.thecatapi.com/v1/images/search', { signal })
   return res.json()
+}
+
+export async function generateMetadata() {
+  const data = await getData()
+  return {
+    title: data[0].id
+  }
 }
 
 export default async function Page() {
   const data = await getData()
+  return (
+    <>
+      <h1>图片 ID：{data[0].id}</h1>
+      <img src={data[0].url} width="300" />
+      <CatDetail />
+    </>
+  )
+}
 
-  return <img src={data[0].url} width="300" />
+async function CatDetail() {
+  const data = await getData()
+  return (
+    <>
+      <h1>图片 ID：{data[0].id}</h1>
+      <img src={data[0].url} width="300" />
+    </>
+  )
 }
